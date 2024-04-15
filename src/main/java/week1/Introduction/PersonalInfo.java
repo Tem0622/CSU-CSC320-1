ca79267a39c14f39461c4e80da6542dd421dd89e
Temuujin-Tsogtgerel <asutemka@gmail.com>
2024-04-15 16:09:25 -0500

    v1

---
+++ src/main/java/week1/Introduction/PersonalInfo.java
@@ -0,0 +1,20 @@
+package week1.Introduction;
+public class PersonalInfo {
+    public static void main(String[] args) {
+        // Variables for the person's details
+        String firstName = "Temuujin";
+        String lastName = "Tsogtgerel";
+        String streetAddress = "17347 Merigold";
+        String city = "Conroe";
+        String state = "Texas";
+        String zipCode = "77302";
+
+        // Printing each detail on a separate line
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Street Address: " + streetAddress);
+        System.out.println("City: " + city);
+        System.out.println("State: " + state);
+        System.out.println("Zip Code: " + zipCode);
+    }
+}
